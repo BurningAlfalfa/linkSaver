a421a81b0f9c87258b484f19b6d7b7b8b99073b3
BurningAlfalfa <acmonahan@gmail.com>
2019-10-04 17:00:30 -0500

    moved post to index.js LEONAR

--- src/server/index.js
+++ src/server/index.js
@@ -10,4 +10,7 @@ app.use(bodyParser.json());
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
+app.post("/request-verification", (req, res) => {
+    //let { email: value } = req.body;
+    console.log(req.body)
+});
--- src/server/verification.js
+++
@@ -1,4 +0,0 @@
-app.post("/request-verification", (req, res) => {
-    //let { email: value } = req.body;
-    console.log(req.body)
-});
